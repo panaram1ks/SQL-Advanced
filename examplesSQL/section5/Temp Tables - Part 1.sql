1f8f5862425152a46bbd6bce3fb889f028b83205
parom <E.Parominskiy@isida.by>
2026-01-28 12:03:38 +0300

    Temp Tables - Part 1

---
+++ examplesSQL/section5/Temp Tables - Part 1.sql
@@ -0,0 +1,25 @@
+SELECT
+    soh.OrderDate ,
+    soh.TotalDue ,
+    DATEFROMPARTS(YEAR(soh.OrderDate),MONTH(soh.OrderDate), 1) AS OrderMonth,
+    ROW_NUMBER() OVER(PARTITION BY DATEFROMPARTS(YEAR(soh.OrderDate),MONTH(soh.OrderDate), 1) ORDER BY soh.TotalDue DESC) AS OrderRunk
+INTO #TempSales
+FROM Sales.SalesOrderHeader soh
+
+SELECT
+    OrderMonth,
+    SUM(TotalDue) AS Top10Due
+INTO #Top10
+FROM #TempSales
+WHERE OrderRunk <= 10
+GROUP BY OrderMonth
+
+
+SELECT
+    A.OrderMonth ,
+    A.Top10Due ,
+    B.Top10Due AS PrevTop10Due
+FROM #Top10 A
+    LEFT JOIN #Top10 B
+ON A.OrderMonth = DATEADD(MONTH, 1, B.OrderMonth )
+ORDER BY A.OrderMonth
